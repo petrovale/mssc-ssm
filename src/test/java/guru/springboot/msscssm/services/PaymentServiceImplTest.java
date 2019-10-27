8631e11a87bc50d8be9320f9af9ef2c01ef59c3e
petrovale <leha.isakov@yandex.ru>
2019-10-27 14:09:50 +0300

    Add Action for PRE_AUTH to AUTH or AUTH_ERROR and add Test for NEW to AUTH / AUTH_ERROR

--- src/test/java/guru/springboot/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/guru/springboot/msscssm/services/PaymentServiceImplTest.java
@@ -8,6 +8,7 @@ import guru.springboot.msscssm.domain.PaymentState;
 import guru.springboot.msscssm.repository.PaymentRepository;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,5 +48,23 @@ class PaymentServiceImplTest {
 
     System.out.println(preAuthedPayment);
 
+  }
+
+  @Transactional
+  @RepeatedTest(10)
+  void testAuth() {
+    Payment savedPayment = paymentService.newPayment(payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+    if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+      System.out.println("Payment is Pre Authorized");
+
+      StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+      System.out.println("Result of Auth: " + authSM.getState().getId());
+    } else {
+      System.out.println("Payment failed pre-auth...");
+    }
   }
 }
